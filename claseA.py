ff08de8bb09c67cf646390d90b25799b0a782bcd
CosmeMiralles <cosme.upf@gmail.com>
2023-06-21 10:36:03 +0200

    adición de la clase A

---
+++ claseA.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 21 10:29:31 2023
+
+@author: cosmemirallescarol
+"""
+
+class claseA:
+  def __init__(self, foo, bar):
+    self.foo = foo
+    self.bar = bar
+
+cA = claseA("foo", "bar")
+
+print(cA.foo)
+print(cA.bar)
+
