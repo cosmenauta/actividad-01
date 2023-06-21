7e0ce8802a3ce2f32703bde8ed4b5eb7712b58f2
CosmeMiralles <cosme.upf@gmail.com>
2023-06-21 10:54:00 +0200

    añadido del atributo lorem en clase A

--- claseA.py
+++ claseA.py
@@ -7,12 +7,15 @@ Created on Wed Jun 21 10:29:31 2023
 """
 
 class claseA:
-  def __init__(self, foo, bar):
+  def __init__(self, foo, bar, lorem):
     self.foo = foo
     self.bar = bar
+    self.lorem = lorem
 
-cA = claseA("foo", "bar")
+cA = claseA("foo", "bar", "lorem")
 
 print(cA.foo)
 print(cA.bar)
+print(cA.lorem)
+
 
